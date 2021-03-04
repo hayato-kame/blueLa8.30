25b77299ebfd7c9eab3010f12bf71a57efd38747
hayato-kame <66853293+hayato-kame@users.noreply.github.com>
2021-03-04 18:26:59 +0900

    component

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,28 +1,17 @@
-<html>
-<head>
-   <title>Hello/Index</title>
-   <style>
-   body {font-size:16pt; color:#999; }
-   h1 { font-size:50pt; text-align:right; color:#f6f6f6;
-       margin:-20px 0px -30px 0px; letter-spacing:-4pt; }
-   </style>
-</head>
-<body>
-   <h1>Blade/Index</h1>
-   
-@if ($msg != '')
-    <p>こんにちは {{$msg}} さん</p>
-    
-@else
-    名前を書いてください。
+@extends('layouts.helloapp')
 
-@endif
+@section('title', 'Index')
 
- <form method="POST" action="/hello">
-    @csrf 
-    <input type="text" name="input">
-    <input type="submit">
- </form>
- 
-</body>
-</html>
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです</p>
+@endsection
+
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
